a7e3cf6918019ac06216df60a665c063563916cc
Andras Dobreff <Andras_Istvan_Dobreff@epam.com>
2022-12-06 13:06:46 +0100

    Day6 A

---
+++ day6_a.py
@@ -0,0 +1,8 @@
+puzzle_input = open("./day6_a_puzzle_input.txt", "r").read()
+
+def find_first_four_different_index(datastream):
+    for i in range(0, len(datastream)-3):
+        if(len(set(datastream[i:i+4])) == 4):
+            return i+4
+
+print(find_first_four_different_index(puzzle_input))
